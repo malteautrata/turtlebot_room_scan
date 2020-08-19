085d49a28923ddb7897c0451456948b88dab3e37
malteautrata <malteautrata@icloud.com>
2020-08-19 17:46:32 +0200

    Get the position of the turtlebot

--- src/room_scan.cpp
+++ src/room_scan.cpp
@@ -4,23 +4,38 @@
 #include "sensor_msgs/LaserScan.h"
 #include "nav_msgs/Odometry.h"
 
+struct startPosition
+{
+    double x;
+    double y;
+};
+
 void callback();
 ros::Publisher speedPub;
 ros::Subscriber scanSub;
 ros::Subscriber odomSub;
 
+double xPos;
+double yPos;
+startPosition startPos;
+bool startReached = false;
 
 void callbackScan(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
-    std::cout << msg->ranges[0];
-
+    std::cout << msg->ranges[90]; //herausfinden was der Wert bei 90° entspricht
+   
     geometry_msgs::Twist twist;
 
-    if(msg->ranges[0] <= 0.3)
+    if(msg->ranges[90] <= 0.3)  
     {
         twist.linear.x = 0.0; twist.linear.y = 0.0; twist.linear.z = 0.0;
         twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = 90.0;
     } else {
+        if (yPos < startPos.y)
+        {
+            startPos.x = xPos;
+            startPos.y = yPos;
+        }
         twist.linear.x = 0.5; twist.linear.y = 0.0; twist.linear.z = 0.0;
     }
     pub.publish(twist);
@@ -28,7 +43,11 @@ void callbackScan(const sensor_msgs::LaserScan::ConstPtr& msg)
 
 void callbackOdom(const nav_msgs::Odometry::ConstPtr& odom)
 {
+    std::cout << odom->pos.pose.pose.position.x;
+    std::cout << odom->pos.pose.pose.position.y;
 
+    xPos = odom.pose.pose.position.x;
+    yPos = odom.pose.pose.position.y;
 }
 
 int main(int argc, char** argv){
