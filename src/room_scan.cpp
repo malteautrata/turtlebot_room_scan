361f96469d70efe5efa4ec1d2e3264adb1990a78
malteautrata <malteautrata@icloud.com>
2020-09-27 10:49:49 +0200

    Erstellung einer Grundlage

--- src/room_scan.cpp
+++ src/room_scan.cpp
@@ -2,86 +2,117 @@
 #include <tf/transform_broadcaster.h>
 #include "geometry_msgs/Twist.h"
 #include "sensor_msgs/LaserScan.h"
-#include "nav_msgs/Odometry.h"
-#include "sensor_msgs/Imu.h"
-
-struct position
-{
-    double x;
-    double y;
-};
+#include <thread>         
 
 void callback();
-ros::Publisher twistPub;
-ros::Subscriber scanSub;
-ros::Subscriber odomSub;
-ros::Subscriber imuSub;
-
-//Der Abstand, zu dem die Wand abgefahren werden soll
-double distanceToWall = 0.4;
-
-//Startposition merken
-position startPos;   
-position currentPos;
-
-//evtl. nicht benötigt
-bool roomScanned = false;
-
-// herausfinden, wann der Raum koomplett gescannt wurde
-
-void callbackScan(const sensor_msgs::LaserScan::ConstPtr& msg)
+ros::Publisher pub;
+ros::Subscriber sub;
+bool shouldDodge = false;
+bool isTurning;
+bool turnLeft;
+bool randomBool()
 {
-    //std::cout << msg->ranges[270]; //herausfinden was der Wert bei 90° entspricht
-   
+    return rand() % 2;
+}
+
+void callback(const sensor_msgs::LaserScan::ConstPtr& msg)
+{
     geometry_msgs::Twist twist;
 
+    // Herausfinden wieviele Daten im angle[] array gespeichert sind: von 0 bis 359
+    //int num_readings = std::ceil( (msg->angle_max - msg->angle_min) / msg->angle_increment );
+    //std::cout << num_readings << std::endl;
+    //std::cout << "Range 350:" <<  msg->ranges[350] << std::endl;
+
+
+    // Herausfinden welche bei der Ausweichung sinnvoll sind
     /*
-    if (currentPos.x == startPos.x)
+    for (int i = 0; i <= 359; i++)
     {
-        distanceToWall += 1;
+        if (msg->ranges[i] <= 0.3 && msg->ranges[i] >= 0.00001)
+        {
+            std::cout << i << std::endl;
+        } 
     }
+    std::cout << "new" << std::endl;
     */
 
-    //Turtlebot darf nicht mehr als 90° drehen, damit er nicht zurück fährt
-    if(msg->ranges[270] <= distanceToWall && msg->ranges[0] >= 0.3)  
+    shouldDodge = false;
+
+    for(int i = 1; i <= 45; i++)
+    {
+        if (msg->ranges[i] <= 0.15 && msg->ranges[i] >= 0.0001)
+        {
+            shouldDodge = true;
+        }
+    }
+
+    for(int i = 315; i <= 359; i++)
+    {
+        if (msg->ranges[i] <= 0.15 && msg->ranges[i] >= 0.0001)
+        {
+            shouldDodge = true;
+        }
+    }
+
+    if (msg->ranges[0] <= 0.2)
+    {
+        shouldDodge = true;
+    }
+
+    if(shouldDodge)
+    {
+        twist.linear.x = 0.0; twist.linear.y = 0.0; twist.linear.z = 0.0;
+        std::cout << randomBool() << std::endl;
+
+        if (isTurning)
+       {
+
+       } else
+       {
+           turnLeft = randomBool();
+       }
+       
+       isTurning = true;
+
+        if(turnLeft)
+        {
+            twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = 5.0;
+
+        } else 
+        {
+            twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = -5.0;
+
+        }
+    } else 
     {
         twist.linear.x = 0.5; twist.linear.y = 0.0; twist.linear.z = 0.0;
-    } else {
-        
-        twist.linear.x = 0.0; twist.linear.y = 0.0; twist.linear.z = 0.0;
-        twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = -10.0;
+        isTurning = false;
     }
-    twistPub.publish(twist);
+    pub.publish(twist);
 }
 
-void callbackOdom(const nav_msgs::Odometry::ConstPtr& odom)
+void launchSlam()
 {
-    //std::cout << "X:" << odom->pose.pose.position.x << std::endl;
-    //std::cout << "Y:" << odom->pose.pose.position.y << std::endl;
-
-    currentPos.x = odom->pose.pose.position.x;
-    currentPos.y = odom->pose.pose.position.y;
-
-    if (currentPos.y < startPos.y)
-    {
-        startPos.x = currentPos.x;
-        startPos.y = currentPos.y;
-    }
+    system("roslaunch turtlebot3_slam turtlebot3_slam.launch slam_methods:=gmapping");
 }
 
-void callbackImu(const sensor_msgs::Imu::ConstPtr& imu)
+// wird nicht genutzt
+void cleanShutdown()
 {
-    //Read out rotation
-    //std::cout << imu->angular_velocity << std::endl;
+    
 }
 
 int main(int argc, char** argv){
-    ros::init(argc, argv, "room_scan");
+    time_t seconds;
+    time(&seconds);
+    srand((unsigned int) seconds);
+    ros::init(argc, argv, "drive_random");
     ros::NodeHandle n;
-    twistPub = n.advertise<geometry_msgs::Twist>("cmd_vel", 10);
-    scanSub = n.subscribe("/scan", 10, callbackScan);
-    odomSub = n.subscribe("/odom", 10, callbackOdom);
-    imuSub = n.subscribe("/imu", 10, callbackImu);
+    pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 10);
+    sub = n.subscribe("/scan", 10, callback);
+
+    std::thread slamThread (launchSlam); 
 
     while(ros::ok())
     {
